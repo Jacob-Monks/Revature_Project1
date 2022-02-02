3b8bb59a3f663b1bf2a97e91dcdfe5d10bbc31d3
Jacob-Monks <97700113+Jacob-Monks@users.noreply.github.com>
2022-02-02 16:18:54 -0500

    Project was created, text files are created, program was run to be sure it works.

---
+++ build.sbt
@@ -0,0 +1,15 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.11.12"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "Revature_Project1"
+  )
+//spark-dependencies
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.1"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.1"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-hive
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.3.1"
---
+++ src/main/scala/Project1_app.scala
@@ -0,0 +1,27 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
+
+object Project1_app {
+  def main(args: Array[String]): Unit = {
+    // create a spark session
+    // for Windows
+    System.setProperty("hadoop.home.dir", "C:\\winutils")
+
+    val spark = SparkSession.builder()
+      .appName("HiveTest5")
+      .config("spark.master", "local")
+      .enableHiveSupport()
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    println("created spark session")
+    //spark.sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING) USING hive")
+    //spark.sql("CREATE TABLE IF NOT EXISTS src(key INT, value STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ‘,’ STORED AS TEXTFILE")
+    //spark.sql("LOAD DATA LOCAL INPATH 'input/kv1.txt' INTO TABLE src")
+    //spark.sql("CREATE TABLE IF NOT EXISTS src (key INT,value STRING) USING hive")
+    
+    //when creating a new table, change newone# otherwise an error will appear that table already exists.
+    spark.sql("create table newone2(Beverage String, Branch String) row format delimited fields terminated by ','");
+    spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_Branch.txt' INTO TABLE newone2")
+    spark.sql("SELECT * FROM newone2").show()
+  }
+}
